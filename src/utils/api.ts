367befd8f6e0b74316310195d9261a95f0aeeef8
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2024-03-14 09:50:21 +0100

    feat: updated config

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,12 +4,11 @@
  *
  * We also create a few inference helpers for input and output types.
  */
+import { type AppRouter } from "@src/server/api/root";
 import { httpBatchLink, loggerLink } from "@trpc/client";
 import { createTRPCNext } from "@trpc/next";
 import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
 import superjson from "superjson";
-
-import { type AppRouter } from "@src/server/api/root";
 
 const getBaseUrl = () => {
   if (typeof window !== "undefined") return ""; // browser should use relative url
@@ -35,7 +34,7 @@ export const api = createTRPCNext<AppRouter>({
        */
       links: [
         loggerLink({
-          enabled: (opts) =>
+          enabled: opts =>
             process.env.NODE_ENV === "development" ||
             (opts.direction === "down" && opts.result instanceof Error),
         }),
