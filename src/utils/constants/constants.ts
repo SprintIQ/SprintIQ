a4dd8f738b63484959400d91aa341953a1bb39b6
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2024-03-24 21:54:12 +0000

    chore: added constants

---
+++ src/utils/constants/constants.ts
@@ -0,0 +1,14 @@
+export const COOKIE_KEY = "sprintiq::session_token";
+
+export const LABELS = {
+  "change-wallet": "Change wallet",
+  connecting: "Connecting ...",
+  "copy-address": "Copy address",
+  copied: "Copied",
+  disconnect: "Disconnect",
+  "has-wallet": "Connect",
+  "no-wallet": "Select Wallet",
+} as const;
+export enum Routes {
+  GAME = "game",
+}
