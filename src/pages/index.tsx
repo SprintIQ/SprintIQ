e9d0c4b2d6cf1e74e34aabf2859792c2e09e0106
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2024-04-05 09:35:32 +0100

    feat: properly implemented design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,16 @@
+import GetStarted from "@src/components/icons/GetStarted.icon";
 import type { NextPage } from "next";
-import FrameComponent1 from "../components/frame-component1";
-import FrameComponent from "../components/frame-component";
+
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
 
 const LandingPage: NextPage = () => {
   return (
-    <div className="w-full relative [background:linear-gradient(180deg,_#0e2615,_#0f0f0f)] overflow-hidden flex flex-col items-start justify-start pt-2 px-0 pb-[124px] box-border gap-[175px] tracking-[normal] lg:gap-[87px_175px] mq450:gap-[44px_175px]">
-      <FrameComponent1 />
-      <section className="self-stretch flex flex-row items-start justify-center py-0 px-5 box-border max-w-full">
-        <div className="w-[1476px] flex flex-col items-start justify-start gap-[63px] max-w-full mq450:gap-[16px_63px] mq825:gap-[31px_63px]">
-          <FrameComponent />
-          <div className="self-stretch h-[170px] flex flex-row items-start justify-center py-0 px-5 box-border">
-            <img
-              className="h-[170px] w-[170px] relative"
-              loading="lazy"
-              alt=""
-              src="/group-1147.svg"
-            />
-          </div>
-        </div>
+    <div className="relative  flex w-full flex-col  tracking-[normal]">
+      <Navbar />
+      <section className="mx-auto mt-16 flex w-full flex-col items-center">
+        <Hero />
+        <GetStarted className="mt-8 w-36" />
       </section>
     </div>
   );
