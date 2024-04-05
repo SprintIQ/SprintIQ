253d8a698fa322f26700065d055c3d7c48feff3a
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2024-04-05 09:47:31 +0100

    feat: created color scheme for tailwindcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,15 +8,21 @@ export default {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(#013E0A,#007205)",
+      },
       colors: {
         primary: {
           700: "#0F0F0F",
         },
         secondary: {
           700: "#1FC04D",
+          300: "#175611",
         },
         grey: {
           300: "#D9D9D9",
+          200: "#787676",
+          100: "#373737",
         },
       },
     },
