6976b98e35837573ea25054733765b5b2f3c9818
timadigwe <timothyadigwe2019@gmail.com>
2024-05-18 12:51:32 +0100

    ui fix

--- src/components/atoms/Create.tsx
+++ src/components/atoms/Create.tsx
@@ -256,10 +256,10 @@ const CreateGame: NextPage = () => {
                     </div>
 
                     <CorrectOptionInput
+                      questionIndex={questionIndex}
+                      options={question.options}
                       value={question.answer}
-                      onChange={e =>
-                        handleCorrectOptionChange(questionIndex, e.target.value)
-                      }
+                      onChange={handleCorrectOptionChange}
                     />
 
                     <TimerInput
@@ -369,17 +369,79 @@ const OptionInput: React.FC<{
 
 // Component for setting the correct option
 const CorrectOptionInput: React.FC<{
+  questionIndex: number;
+  options: string[];
   value: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-}> = ({ value, onChange }) => (
-  <input
-    type="text"
-    className="relative flex w-[180px]  items-center justify-center bg-transparent text-[1.25rem] leading-[22.53px] text-secondary-700 placeholder-[#373737] outline-none hover:text-[#1FC04D] hover:placeholder-[#1FC04D] "
-    placeholder="Set Correct Option"
-    value={value}
-    onChange={onChange}
-  />
-);
+  onChange: (questionIndex: number, value: string) => void;
+}> = ({ questionIndex, options, value, onChange }) => {
+  const [showOptionsOverlay, setShowOptionsOverlay] = useState<boolean>(false);
+  //console.log("options", options);
+
+  const onClick = () => {
+    if (options.length === 1 && options[0] === "") {
+      toast("No Option has been added");
+    } else {
+      setShowOptionsOverlay(true);
+    }
+  };
+
+  const handleOptionSelect = (selectedOption: string) => {
+    onChange(questionIndex, selectedOption);
+    setShowOptionsOverlay(false); // Hide the options overlay after selection
+  };
+
+  return (
+    <div>
+      <button
+        onClick={onClick}
+        className="relative flex w-[180px] items-center justify-center bg-transparent text-[1.25rem] leading-[22.53px] text-secondary-700 outline-none hover:text-[#1FC04D]"
+      >
+        {value ? value : "Set Correct Option"}
+      </button>
+      {showOptionsOverlay && (
+        <div className="fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center bg-gray-500 bg-opacity-50">
+          <div className="w-full max-w-md rounded bg-gray-800 p-8 shadow-lg">
+            <div className="flex justify-end">
+              <button
+                onClick={() => setShowOptionsOverlay(false)}
+                className="text-gray-500 hover:text-gray-600"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
+            <p className="mb-4 text-center text-[16px] text-[#D9D9D9] ">
+              Select the correct option
+            </p>
+            <div className="flex flex-col items-center">
+              {options.map((option, index) => (
+                <button
+                  key={index}
+                  onClick={() => handleOptionSelect(option)}
+                  className="mb-2 w-full rounded-[12px] bg-green-500 px-6 py-2 text-white hover:bg-green-600"
+                >
+                  {option}
+                </button>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 // Component for setting the timer
 const TimerInput: React.FC<{
