52479c21bd5c83640faa3dcbaab2a9e16019840a
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2024-07-21 21:06:27 +0100

    feat: added global 404 page

--- src/components/atoms/404.tsx
+++ src/components/atoms/404.tsx
@@ -3,17 +3,18 @@ import Image from "next/image";
 import Link from "next/link";
 import * as React from "react";
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface I404Props {}
+export interface I404Props {
+  isRoot: boolean;
+}
 
-const Page404: React.FC<I404Props> = () => {
+const Page404: React.FC<I404Props> = ({ isRoot }) => {
   return (
     <section className="flex h-full min-h-screen w-full flex-col items-center justify-center space-y-5">
       <Image src="/404.png" alt="404" width={200} height={200} />
       <h2 className="text-4xl font-bold">404 Error</h2>
       <p className="text-xl">Sorry, page not found</p>
       <Link
-        href={`/dashboard/${Routes.HOME}`}
+        href={isRoot ? "/" : `/dashboard/${Routes.HOME}`}
         className="rounded-2xl border border-secondary-700 bg-secondary-300/10 px-8 py-4 text-white transition-colors duration-500 hover:bg-secondary-300"
       >
         Back to home
