8f849a95378bbacf82d190be1e5e4d1de9c79357
francis-codex <francislogo2219@gmail.com>
2024-07-25 00:34:29 +0100

    feat: metadata added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -116,7 +116,8 @@ const MyApp: AppType = ({ Component, pageProps }) => {
     // add toast here
   };
   return (
-    <ConnectionProvider endpoint={endpoint}>
+    <>
+        <ConnectionProvider endpoint={endpoint}>
       <WalletProvider
         wallets={wallets}
         localStorageKey={COOKIE_KEY}
@@ -140,6 +141,76 @@ const MyApp: AppType = ({ Component, pageProps }) => {
         </WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
+
+    <Helmet>
+    <meta name="author" content="Francis Codex"/>
+    <meta
+        name="description"
+        content="Conquer Quizzes, Expand Knowledge, and Thrive in the Blockchain Revolution."
+    />
+    <meta
+        name="keywords"
+        content="SprintIQ, Connect Engage, Earn, Game, Web3, Solana, Learn, Learn and Earn"
+    />
+    <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
+
+    <title>
+      SprintIQ - Connect Engage, Earn
+    </title>
+    <meta property="og:url" content="https://sprintiq-landing.vercel.app"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:title" content="SprintIQ"/>
+    <meta
+        property="og:description"
+        content="Conquer Quizzes, Expand Knowledge, and Thrive in the Blockchain Revolution."
+    />
+    <meta
+        property="og:image"
+        content="https://sprintiq.fun/og_image.jpg"
+    />
+    <meta name="twitter:card" content="summary_large_image"/>
+    <meta property="twitter:domain" content="https://sprintiq-landing.vercel.app"/>
+    <meta
+        property="twitter:url"
+        content="https://sprintiq-landing.vercel.app"
+    />
+    <meta name="twitter:title" content="SprintIQ"/>
+    <meta
+        name="twitter:description"
+        content="SprintIQ, Conquer Quizzes, Expand Knowledge, and Thrive in the Blockchain Revolution."
+    />
+    <meta
+        name="twitter:image"
+        content="https://sprintiq.fun/og_image.jpg"
+    />
+
+    <meta property="og:title" content="SprintIQ"/>
+    <meta property="og:type" content="website"/>
+    <meta name="theme-color" content="#187318"/>
+
+    <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicon/apple-touch-icon.png"
+    />
+    <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon/favicon-32x32.png"
+    />
+    <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon/favicon-16x16.png"
+    />
+    <link rel="icon" href="/favicon.ico" type="image/x-icon"></link>
+    <link rel="manifest" href="/favicon/site.webmanifest"/>
+    <meta name="msapplication-TileColor" content="#187318"/>
+  </Helmet>
+  
+  </>
   );
 };
 
