e41aa12a81a7f382471a2246bb8ba4911f650e1c
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2024-07-30 21:52:59 +0100

    chore: fixed eslint issues

--- src/components/ui/ModalWrapper.tsx
+++ src/components/ui/ModalWrapper.tsx
@@ -1,26 +1,27 @@
-import React, {FC, type ReactNode, useState} from "react";
-import {WalletModalContext, WalletModalProps} from "@solana/wallet-adapter-react-ui";
+import {
+  WalletModalContext,
+  type WalletModalProps,
+} from "@solana/wallet-adapter-react-ui";
 import Modal from "@src/components/ui/Modal";
+import React, { type FC, type ReactNode, useState } from "react";
 
 export interface ModalWrapperProps extends WalletModalProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
-const ModalWrapper: FC<ModalWrapperProps> = (props) => {
-    const [visible, setVisible] = useState(false);
-    return (
-        <WalletModalContext.Provider
-            value={{
-                visible,
-                setVisible,
-            }}
-        >
-            {props.children}
-            {visible && <Modal/>}
-
-        </WalletModalContext.Provider>
-    );
+const ModalWrapper: FC<ModalWrapperProps> = props => {
+  const [visible, setVisible] = useState(false);
+  return (
+    <WalletModalContext.Provider
+      value={{
+        visible,
+        setVisible,
+      }}
+    >
+      {props.children}
+      {visible && <Modal />}
+    </WalletModalContext.Provider>
+  );
 };
 
 export default ModalWrapper;
-
